03ac31d89ef35270875b41b9b95d09fec0674b9f
heonjwa <heonjwa@gmail.com>
2024-01-29 02:12:41 -0800

    navbar started

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Button from '@mui/material/Button';
 import { Outlet, Link } from "react-router-dom";
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <header className="App-header">
         <h1> DubX </h1>
         <img src={logo} className="App-logo" alt="logo" />
--- my-app/src/login.js
+++ my-app/src/login.js
@@ -2,10 +2,12 @@ import './App.css';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { Outlet, Link } from "react-router-dom";
+import Navbar from './Components/Navbar';
 
 function Login() {
   return (
     <div className="App">
+        <Navbar></Navbar>
         <h1> Login </h1>
         <TextField id="outlined-basic" label="Outlined" variant="outlined" />
         <TextField id="outlined-basic" label="Outlined" variant="outlined" />
